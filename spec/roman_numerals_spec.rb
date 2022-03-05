1084f45da5ad77b9dc50ce49ab7dcb745d097412
RaccoonHowell <rac777blue@hotmail.com>
2022-03-05 17:34:15 +0000

    failed test "returns V when passed 5"

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -6,18 +6,22 @@ describe Roman_Numerals do
     end
 
     it "returns 'I' when passed 1" do
-        expect(@roman_numerals.romanize(1)).to eq('I')
+        expect(@roman_numerals.romanize(1)).to eq("I")
     end
 
     it "returns 'II' when passed 2" do
-        expect(@roman_numerals.romanize(2)).to eq('II')
+        expect(@roman_numerals.romanize(2)).to eq("II")
     end
 
     it "returns 'III' when passed 3" do
-        expect(@roman_numerals.romanize(3)).to eq('III')
+        expect(@roman_numerals.romanize(3)).to eq("III")
     end
 
     it "returns 'IV' when passed 4" do
-        expect(@roman_numerals.romanize(4)).to eq('IV')
+        expect(@roman_numerals.romanize(4)).to eq("IV")
+    end
+
+    it "returns 'V' when passed 5" do
+        expect(@roman_numerals.romanize(5)).to eq("V")
     end
 end
