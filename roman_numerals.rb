bde565b9096b22aff9f27cf15dba4dac1caacc8c
RaccoonHowell <rac777blue@hotmail.com>
2022-03-05 17:21:37 +0000

    passed test "returns III when passed 3"

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,6 +1,8 @@
 class Roman_Numerals
     def romanize number
-        if number == 2
+        if number == 3
+            "III"
+        elsif number == 2
             "II" 
         else
             "I"
