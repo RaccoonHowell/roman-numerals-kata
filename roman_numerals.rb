aa5247ae1fd79f9dbcf7eb90119fad92550de12f
RaccoonHowell <rac777blue@hotmail.com>
2022-03-05 17:32:02 +0000

    passed test "returns IV when passed 4"

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -2,10 +2,14 @@ class Roman_Numerals
     def romanize number
         romanized_string = ""
 
-        number.times do 
-            romanized_string += "I"
+        if number == 4
+            romanized_string += "IV"
+        else
+            number.times do 
+                romanized_string += "I"
+            end
         end
-        
+
         romanized_string
     end
 end
