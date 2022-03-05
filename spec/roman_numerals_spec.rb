62e6a50c5fd629eb9e43a3cb43879d732df0af9c
RaccoonHowell <rac777blue@hotmail.com>
2022-03-05 17:13:46 +0000

    failed test "returns II when passed 2"

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -8,4 +8,8 @@ describe Roman_Numerals do
     it "returns 'I' when passed 1" do
         expect(@roman_numerals.romanize(1)).to eq('I')
     end
+
+    it "returns 'II' when passed 2" do
+        expect(@roman_numerals.romanize(2)).to eq('II')
+    end
 end
