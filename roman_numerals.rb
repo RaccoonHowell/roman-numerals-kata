aecddf2384d0b807cf2c8b42cce8d0f87fc0dd6a
RaccoonHowell <rac777blue@hotmail.com>
2022-03-07 20:04:35 +0000

    passed test "turns 100 into C"

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,14 +1,15 @@
 class Roman_Numerals
     def romanize number
         numerals = {
-            90 => "XC",
-            50 => "L",
-            40 => "XL",
-            10 => "X",
-            9  => "IX",
-            5  => "V",
-            4  => "IV",
-            1  => "I"
+            100 => "C",
+            90  => "XC",
+            50  => "L",
+            40  => "XL",
+            10  => "X",
+            9   => "IX",
+            5   => "V",
+            4   => "IV",
+            1   => "I"
         }
 
         romanized_string = ""
